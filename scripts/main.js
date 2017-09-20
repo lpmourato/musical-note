ef44977baab49d96eb75e07c9098dd732b1c6aa4
Patricio <patricio17br@gmail.com>
2017-09-20 00:00:11 -0300

    Create buttons of key notes to match with sounds
    - Change routes to handle same requests from frontend
    - Create buttons to match key notes with sounds
    - Create score

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,31 @@
 const BASS = 'bass';
 const GUITAR = 'guitar';
 const PIANO = 'piano';
+const ALL = 'all';
 
 let instrument = 'guitar/?defaultKey';
 let loop = null;
+let currentKey = '';
+let score = 0;
+
 function changeInstrumentBass(event) {
     instrument = BASS;
-    playSound('bass/?defaultKey=a');
+    playSound('bass/?defaultKey=a2.mp3');
 }
 
 function changeInstrumentGuitar(event) {
     instrument = GUITAR;
-    playSound('guitar/?defaultKey=a');
+    playSound('guitar/?defaultKey=a2.mp3');
 }
 
 function changeInstrumentPiano(event) {
     instrument = PIANO;
-    playSound('piano/?defaultKey=a');
+    playSound('piano/?defaultKey=a2.mp3');
+}
+
+function changeInstrumentAll(event) {
+    instrument = ALL;
+    playSound('all');
 }
 
 function play() {
@@ -32,7 +41,10 @@ function playSound(keyNote) {
     document.body.appendChild(sound);
     xhttp.onreadystatechange = function() {
         if (xhttp.readyState === 3 && xhttp.status === 200) {
-            sound.src = xhttp.response;
+            console.log(JSON.parse(xhttp.response))
+            const response = JSON.parse(xhttp.response);
+            sound.src = response.address;
+            currentKey = response.key;
             sound.play();
         }
     }
@@ -61,4 +73,13 @@ function load() {
     }
     xhttp.open("GET", 'http://localhost:5000/load', true)
     xhttp.send();
+}
+
+function match(key) {
+    const matchKeys = currentKey.replace(/[0-9]/g, '') === key.toLowerCase()
+    if (matchKeys) {
+        const spanScore = document.getElementById('score');
+        spanScore.textContent += 1;
+    }
+    currentKey = '';
 }
